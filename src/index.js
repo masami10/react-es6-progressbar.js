8c2c7fb6a0b5b948f48dbc7ac0fbc16c50d6a8c3
masami10 <masami.nagasawa10@hotmail.com>
2018-08-02 16:08:56 +0800

    添加调试

--- src/index.js
+++ src/index.js
@@ -5,25 +5,26 @@ import ProgressBar from 'progressbar.js';
 let shape = null;
 
 class Shape extends Component{
-  
+
   constructor(){
-    
+
     super();
-    
+
     this.progressBar = createRef();
-    
+
   }
 
   componentWillUpdate() {
     if (shape != null && this.props.startAnimate) {
+      　console.log('update');
         shape.animate(this.props.progress >= 0 ? this.props.progress : 0.5);
     }
   }
-  
+
   componentDidMount(){
-    
+
     let ShapeType, text_val;
-    
+
     if(this.props.type === 'line'){
       ShapeType = ProgressBar.Line;
     } else if(this.props.type === 'semicircle'){
@@ -31,51 +32,51 @@ class Shape extends Component{
     } else{
       ShapeType = ProgressBar.Circle;
     }
-    
-    
+
+
     text_val = typeof this.props.options.text === 'string' ? {value: this.props.options.text} : this.props.options.text;
-    
+
     let input_options = this.props.options;
-    
+
     input_options.text = text_val;
-    
+
     shape = new ShapeType(this.progressBar.current, input_options, this.props.callback);
 
   }
-  
+
   render(){
 
-    
+
     return(
-    
+
       <div
         ref={this.progressBar}
         className='progressbar-container'
         style={this.props.container_style}
       ></div>
-    
+
     );
-    
+
   }
-  
+
 }
 
 const Circle = props => (
-  
+
   <Shape type='circle' container_class={props.container_class} options={props.options} {...props} />
-  
+
 );
 
 const SemiCircle = props => (
-  
+
   <Shape type='semicircle' container_class={props.container_class} options={props.options} {...props} />
-  
+
 );
 
 const Line = props => (
-  
+
   <Shape type='line' container_class={props.container_class} options={props.options} {...props} />
-  
+
 );
 
 export { Circle, SemiCircle, Line };
